5dd5ef03b04843547da97055549f104a1702bf19
Marc <jester.ismael@gmail.com>
2025-11-23 12:47:57 -0500

    Added Javascript  for game mode

--- hangman/static/hangman/hangman.js
+++ hangman/static/hangman/hangman.js
@@ -1,7 +1,44 @@
-function easy() {
-    event.preventDefault();
+let easy = ["Noob", "Value", "Dog", "Cat"]
+let medium = ["Hangman", "Random", "Edited", "Course"]
+let hard = ["Difficult", "Avalanche", "Verbatim", "Paragraph"]
+
+
+
+function game_mode(mode) {
+    //Mode bassed on what the user clicked
+    let word = "";
+    if (mode === "easy") {
+        let easy = ["Noob", "Value", "Dog", "Cat"];
+        ran = Math.floor(Math.random() * easy.length);
+        word = easy[ran];
+        console.log("Easy")
+        console.log(word);
+    }
+    else if (mode === "medium") {
+        let medium = ["Hangman", "Random", "Edited", "Course"];
+        ran = Math.floor(Math.random() * medium.length);
+        word = medium[ran];
+        console.log("Medium")
+        console.log(word);
+    }
+    else {
+        let hard = ["Difficult", "Avalanche", "Verbatim", "Paragraph"];
+        ran = Math.floor(Math.random() * hard.length);
+        word = hard[ran];
+        console.log("Hard");
+        console.log(word);
+    }
+
+    //Hide Game Option and Show Game
+    document.getElementById("center").style.display = "none";
+    document.getElementById("center-1").style.display = "flex";
+
+    //Change word to "_"
+    const underscores = "_ ".repeat(word.length);
+    document.getElementById("game-word").innerHTML = underscores;
+
 
     
 
-
 }
+
